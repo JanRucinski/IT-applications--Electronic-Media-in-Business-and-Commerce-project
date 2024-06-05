00344ab0721d31645dc339d9ece32f1194eddd4c
Paweł Wieszczeczyński <wieszczeczynskip@gmail.com>
2024-06-05 17:49:21 +0200

    Added returning reserved dates when fetching rent-items

--- Backend/src/main/java/backend/repository/RentalRepository.java
+++ Backend/src/main/java/backend/repository/RentalRepository.java
@@ -3,5 +3,8 @@ package backend.repository;
 import backend.model.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+    List<Rental> findRentalsByItemId(Long itemId);
 }
