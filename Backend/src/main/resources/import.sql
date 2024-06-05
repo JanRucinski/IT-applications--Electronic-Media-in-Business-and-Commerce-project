b9d0c33e75124623dae6d74f3114bb2fbb5c560b
Paweł Wieszczeczyński <wieszczeczynskip@gmail.com>
2024-06-05 16:40:05 +0200

    Role field in User entity

--- Backend/src/main/resources/import.sql
+++ Backend/src/main/resources/import.sql
@@ -56,8 +56,10 @@ INSERT INTO items (id, name, `desc`, quantity, price, image_url, category_id, cr
 INSERT INTO items (id, name, `desc`, quantity, price, image_url, category_id, created_at, modified_at) VALUES (148, 'Tracer BMX Bike', 'A freestyle BMX for beginners and advanced riders.', 1, 25.00, 'https://tracerbikes.com/cdn/shop/products/shinningblack1.jpg?v=1640307102&width=1600', 48, '2024-05-19 10:30:00 +02:00', '2024-05-19 10:30:00 +02:00');
 
 -- USERS
-INSERT INTO users (id, username, password, email, first_name, last_name, phone, created_at, modified_at) VALUES (100, 'johndoe', '$2a$10$KbQmX0trE/.xj8aJQxIl3uUh3dSO2Qnr6p/qz3lkUae.FUaf0W4ty', 'john.doe@example.com', 'John', 'Doe', 1234567890, '2024-05-19 10:00:00 +02:00', '2024-05-19 10:00:00 +02:00');
-INSERT INTO users (id, username, password, email, first_name, last_name, phone, created_at, modified_at) VALUES (101, 'janedoe', '$2a$10$K6ShwqNlkl3sVz/LlkN8vea3hE/ZlXQ9e7orP78.B1A7kXzB7BF9a', 'jane.doe@example.com', 'Jane', 'Doe', 9876543210, '2024-05-19 11:00:00 +02:00', '2024-05-19 11:00:00 +02:00');
+-- Password: password123
+INSERT INTO users (id, username, password, email, first_name, last_name, phone, role, created_at, modified_at) VALUES (100, 'johndoe', '$2a$10$D8yxdfdf.KbQmX0trE/.xj8aJQxIl3uUh3dSO2Qnr6p/qz3lkUae.FUaf0W4ty', 'john.doe@example.com', 'John', 'Doe', 1234567890, 'ADMIN', '2024-05-19 10:00:00 +02:00', '2024-05-19 10:00:00 +02:00');
+-- Password: password456
+INSERT INTO users (id, username, password, email, first_name, last_name, phone, role, created_at, modified_at) VALUES (101, 'janedoe', '$2a$10$K6ShwqNlkl3sVz/LlkN8vea3hE/ZlXQ9e7orP78.B1A7kXzB7BF9a', 'jane.doe@example.com', 'Jane', 'Doe', 9876543210, 'USER', '2024-05-19 11:00:00 +02:00', '2024-05-19 11:00:00 +02:00');
 
 -- PAYMENTS
 INSERT INTO payments (id, amount, provider, status, created_at, modified_at) VALUES (100, 1400.00, 'PayPal', 'COMPLETED', '2024-05-19 10:40:00 +02:00', '2024-05-19 10:40:00 +02:00');
@@ -72,5 +74,5 @@ INSERT INTO order_items (id, quantity, prize, order_id, item_id, created_at, mod
 INSERT INTO order_items (id, quantity, prize, order_id, item_id, created_at, modified_at) VALUES (101, 1, 1500.00, 101, 102, '2024-05-19 11:50:00 +02:00', '2024-05-19 11:50:00 +02:00');
 
 -- RENTALS
-INSERT INTO rentals (id, rental_start, rental_end, total, status, item_id, user_id, payment_id, created_at, modified_at) VALUES (100, '2024-06-01 08:00:00 +02:00', '2024-06-07 20:00:00 +02:00', 300.00, 'IN_PROGRESS', 141, 100, 100, '2024-05-19 10:00:00 +02:00', '2024-05-19 10:00:00 +02:00');
-INSERT INTO rentals (id, rental_start, rental_end, total, status, item_id, user_id, payment_id, created_at, modified_at) VALUES (101, '2024-06-15 08:00:00 +02:00', '2024-06-20 20:00:00 +02:00', 250.00, 'REQUESTED', 142, 101, 101, '2024-05-19 11:00:00 +02:00', '2024-05-19 11:00:00 +02:00');
+INSERT INTO rentals (id, rental_start, rental_end, total, status, item_id, user_id, payment_id, created_at, modified_at) VALUES (100, '2024-06-01', '2024-06-07', 300.00, 'IN_PROGRESS', 141, 100, 100, '2024-05-19 10:00:00 +02:00', '2024-05-19 10:00:00 +02:00');
+INSERT INTO rentals (id, rental_start, rental_end, total, status, item_id, user_id, payment_id, created_at, modified_at) VALUES (101, '2024-06-15', '2024-06-20', 250.00, 'REQUESTED', 142, 101, 101, '2024-05-19 11:00:00 +02:00', '2024-05-19 11:00:00 +02:00');
