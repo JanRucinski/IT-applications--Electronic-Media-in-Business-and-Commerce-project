108baf9e4d0ea9f0cd4754d01d48f4ff2b3c71c8
Paweł Wieszczeczyński <wieszczeczynskip@gmail.com>
2024-06-06 19:56:11 +0200

    Refactoring

--- Backend/src/main/java/backend/api/AuthenticationController.java
+++ Backend/src/main/java/backend/api/AuthenticationController.java
@@ -23,37 +23,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/auth")
 public class AuthenticationController {
+    private final UserService userservice;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
-  private final UserService userservice;
-
-  private final AuthenticationManager authenticationManager;
-  private JwtUtil jwtUtil;
-
-  @Autowired
-  public AuthenticationController(@Lazy AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserService userservice) {
-    this.authenticationManager = authenticationManager;
-    this.jwtUtil = jwtUtil;
-    this.userservice = userservice;
-  }
-
-  @ResponseBody
-  @RequestMapping(value = "/login", method = RequestMethod.POST)
-  public ResponseEntity<?> login(@RequestBody LoginReq loginReq) {
-    try {
-      Authentication authentication =
-          authenticationManager.authenticate(
-              new UsernamePasswordAuthenticationToken(loginReq.getUsername(), loginReq.getPassword()));
-      String email = authentication.getName();
-      User user = userservice.findUserByUsernameOrEmail(email);
-      String token = jwtUtil.createToken(user);
-      LoginRes loginRes = new LoginRes(email, token);
-      return ResponseEntity.ok(loginRes);
-    } catch (BadCredentialsException e) {
-      ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, "Invalid username or password");
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-    } catch (Exception e) {
-      ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, e.getMessage());
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    @Autowired
+    public AuthenticationController(@Lazy AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserService userservice) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userservice = userservice;
     }
-  }
+
+    @ResponseBody
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<?> login(@RequestBody LoginReq loginReq) {
+        try {
+            Authentication authentication =
+                    authenticationManager.authenticate(
+                            new UsernamePasswordAuthenticationToken(loginReq.getUsername(), loginReq.getPassword()));
+            String email = authentication.getName();
+            User user = userservice.findUserByUsernameOrEmail(email);
+            String token = jwtUtil.createToken(user);
+            LoginRes loginRes = new LoginRes(email, token);
+            return ResponseEntity.ok(loginRes);
+        } catch (BadCredentialsException e) {
+            ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, "Invalid username or password");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        } catch (Exception e) {
+            ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+    }
 }
--- Backend/src/main/java/backend/security/SecurityConfig.java
+++ Backend/src/main/java/backend/security/SecurityConfig.java
@@ -17,56 +17,55 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-  private final UserDetailsService userDetailsService;
-  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
-  private final JwtAuthorizationFilter jwtAuthorizationFilter;
+    public SecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder, JwtAuthorizationFilter jwtAuthorizationFilter) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.jwtAuthorizationFilter = jwtAuthorizationFilter;
+    }
 
-  public SecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder, JwtAuthorizationFilter jwtAuthorizationFilter) {
-    this.userDetailsService = userDetailsService;
-    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    this.jwtAuthorizationFilter = jwtAuthorizationFilter;
-  }
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        return authenticationManagerBuilder.build();
+    }
 
-  @Bean
-  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-    AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    return authenticationManagerBuilder.build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(configurer -> configurer
+                        // Permit all GET requests
+                        .requestMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/items/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/orders/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/rentals/**").permitAll()
+                        // Permit POST requests to /items
+                        .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/items").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/orders").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.POST, "/rentals").hasAnyRole("ADMIN", "USER")
+                        // Restrict PUT requests to ADMIN role
+                        .requestMatchers(HttpMethod.PUT, "/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/items/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/orders/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/rentals/**").hasRole("ADMIN")
+                        // Restrict DELETE requests to ADMIN role
+                        .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/items/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/orders/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/rentals/**").hasRole("ADMIN")
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(configurer -> configurer
-            // Permit all GET requests
-            .requestMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.GET, "/items/**").permitAll()
-            .requestMatchers(HttpMethod.GET, "/orders/**").permitAll()
-            .requestMatchers(HttpMethod.GET, "/rentals/**").permitAll()
-            // Permit POST requests to /items
-            .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.POST, "/items").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.POST, "/orders").hasAnyRole("ADMIN", "USER")
-            .requestMatchers(HttpMethod.POST, "/rentals").hasAnyRole("ADMIN", "USER")
-            // Restrict PUT requests to ADMIN role
-            .requestMatchers(HttpMethod.PUT, "/users/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.PUT, "/items/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.PUT, "/orders/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.PUT, "/rentals/**").hasRole("ADMIN")
-            // Restrict DELETE requests to ADMIN role
-            .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.DELETE, "/items/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.DELETE, "/orders/**").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.DELETE, "/rentals/**").hasRole("ADMIN")
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(Customizer.withDefaults());
 
-            .requestMatchers("/auth/**").permitAll()
-            .anyRequest().authenticated()
-    )
-        .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
-        .httpBasic(Customizer.withDefaults());
-
-    return http.build();
-  }
+        return http.build();
+    }
 }
